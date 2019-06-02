6138dcdfbf7a4349a86db655cfa2baf879a4e992
ProgrammingKiddo <programming.edu@protonmail.com>
2019-06-02 19:14:51 +0200

    Añadida documentación esclarecedora sobre las decisiones de diseño para la función que determina si un grafo es cíclico o no. También se ha corregido la forma de llamar a la función, y se ha añadido la posibilidad de llamar directamente a esGrafoCiclico().

--- P6/esGrafoAciclico.hpp
+++ P6/esGrafoAciclico.hpp
@@ -6,11 +6,29 @@
 #include "../Material/grafoPMC.h"
 #include "../Material/alg_grafo_E-S.h"
 
+/**
+ * Dado que es más sencillo determinar que un grafo sea cíclico, a que
+ * no lo sea, la llamada a "esGrafoAciclico()" devolverá el resultado opuesto
+ * de la comprobación "esGrafoCiclico()".
+ * 
+ * En el momento en que encontramos un ciclo (más de un camino posible entre
+ * nodo A y nodo B) ya sabemos que el grafo es cíclico, y detenemos la búsqueda.
+ * 
+ * Estos métodos solo funcionan para grafos (ponderados o no) NO dirigidos.
+*/
+
 template <typename T>
 bool esGrafoAciclico(const GrafoP<T>& Gponderado)
 {
+    return !esGrafoCiclico(Gponderado);
+}
+
+
+template <typename T>
+bool esGrafoCiclico(const GrafoP<T>& Gponderado)
+{
     vector<bool> visitados(Gponderado.numVert(), false);
-    return !esGrafoCiclico(Gponderado, visitados, 0, 0);
+    return esGrafoCiclico(Gponderado, visitados, 0, 0);
 }
 
 template <typename T>

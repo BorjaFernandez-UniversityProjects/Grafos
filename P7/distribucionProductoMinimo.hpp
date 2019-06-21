85fa3ebf074fe679803cee4a0aff1eb7d36da13d
ProgrammingKiddo <programming.edu@protonmail.com>
2019-06-21 10:01:35 +0200

    Terminado el tercer problema de la práctica 7.

--- P7/distribucionProductoMinimo.hpp
+++ P7/distribucionProductoMinimo.hpp
@@ -11,8 +11,18 @@
 /*  Solución al problema 3 de la segunda relación de problemas de Grafos    */
 
 
-//Precondición:     la función recibe seis parámetros:
-//                   - 
+//Precondiciones:     la función recibe seis parámetros:
+//                   - el vértice que representa el centro de producción en el grafo
+//                   - la cantidad de producto que hay que distribuir en la región
+//                   - la matriz de distancia entre las ciudades de la región
+//                   - un vector que representa la capacidad de almacenamiento de cada ciudad
+//                   - un vector que representa el porcentaje de subvención sobre los costes
+//                      mínimos de cada ciudad
+//                   - un vector de enteros (parámetro de salida) de tamaño igual
+//                      al número de ciudades
+//Postcondición:     en el parámetro de salida se guarda la cantidad de producto a almacenar
+//                      en cada ciudad. Además, la función devuelve el coste total de hacer
+//                      toda la distribución. 
 
 
 template <typename T>
@@ -29,7 +39,8 @@ T distribucionProductoMinimo(typename GrafoP<T>::vertice centroProduccion,
     GrafoP<T> grafoDistanciaCiudades;
     size_t i;
     size_t ciudadRentable;
-    float menorCoste = std::numeric_limits<float>::max();
+    float menorCoste;
+    float costeDistribucion;
 
     grafoDistanciaCiudades = generarGrafo(distanciaCiudades);
     distanciasMinimas = Dijkstra(grafoDistanciaCiudades, centroProduccion, camino);
@@ -43,12 +54,18 @@ T distribucionProductoMinimo(typename GrafoP<T>::vertice centroProduccion,
         i++;
     }
 
+    costeDistribucion = 0;
     i = 0;
+    productoDistribuido = vector<int>(numCiudades, 0);
     while (i < numCiudades && cantidadProducto > 0)
     {
         j = 0;
+        menorCoste = std::numeric_limits<float>::max();
+        // Con este bucle obtenemos en cada iteración la ciudad más rentable no-llena
         while (j < numCiudades)
         {
+            // Si encontramos una ciudad más rentable, distinta del centro de producción
+            // y que no tenga el almacén lleno.
             if (rentabilidadCiudades[j] < menorCoste && j != centroProduccion
                 && ciudadesLlenas[j] == false)
             {
@@ -57,15 +74,25 @@ T distribucionProductoMinimo(typename GrafoP<T>::vertice centroProduccion,
             }
             j++;
         }
-        ciudadesLlenas[ciudadRentable] = true;
-        if (cantidadProducto > almacenamiento[ciudadRentable])
+        // Si el almacenamiento de una ciudad es menor que el producto restante
+        if (cantidadProducto >= almacenamiento[ciudadRentable])
         {
             productoDistribuido[ciudadRentable] = almacenamiento[ciudadRentable];
+            costeDistribucion += rentabilidadCiudades[ciudadRentable] * almacenamiento[ciudadRentable];
             cantidadProducto = cantidadProducto - almacenamiento[ciudadRentable];
         }
-        
+        // Si el almacenamiento de una ciudad es mayor que el producto restante
+        else
+        {
+            productoDistribuido[ciudadRentable] = cantidadProducto;
+            costeDistribucion += rentabilidadCiudades[ciudadRentable] * cantidadProducto;
+            cantidadProducto = 0;
+        }
+        // Marcamos la ciudad actual como llena
+        ciudadesLlenas[ciudadRentable] = true;
+        i++;
     }
-
+    return costeDistribucion;
 }
 
 
